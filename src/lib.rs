1fa1b08b63923ecae6b7def0362c35918c53e0bf
Takahiro <tkhr_621@docomo.ne.jp>
2024-10-25 15:56:04 +0900

    fix comment

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use chrono::prelude::*;
 use wasm_bindgen::prelude::*;
 
-// PC のタイムゾーンを使って現在の日付を yyyy/mm/dd 形式で返す
+// Date in local time yyyy/mm/dd
 #[wasm_bindgen]
 pub fn local_now() -> String {
     let now: DateTime<Local> = Local::now();
