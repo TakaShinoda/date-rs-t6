80377a87599a099777594396463e6377a89192c6
Takahiro <tkhr_621@docomo.ne.jp>
2024-10-27 19:51:27 +0900

    wip: new Date を受け取る関数を作成

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use chrono::prelude::*;
+use js_sys::Date;
 use wasm_bindgen::prelude::*;
 
 // Date in local time yyyy/mm/dd
@@ -6,4 +7,20 @@ use wasm_bindgen::prelude::*;
 pub fn local_now() -> String {
     let now: DateTime<Local> = Local::now();
     now.format("%Y/%m/%d").to_string()
+}
+
+// format local time yyyy/mm/dd hh:mm
+#[wasm_bindgen]
+pub fn format(js_date: &Date) -> String {
+    let timestamp = js_date.get_time() as i64;
+    let datetime_utc: DateTime<Utc> = match DateTime::<Utc>::from_timestamp(timestamp / 1000, 0) {
+        Some(dt) => dt,
+        None => {
+            panic!("Error");
+        }
+    };
+
+    let date: DateTime<Local> = datetime_utc.with_timezone(&Local);
+
+    date.format("%Y/%m/%d %H:%M").to_string()
 }
