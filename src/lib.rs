bf437ecaa7335127fc5c3b17d3e959232c6b7a5a
Takahiro <tkhr_621@docomo.ne.jp>
2024-10-29 21:06:57 +0900

    曜日も返すようにする

--- src/lib.rs
+++ src/lib.rs
@@ -5,5 +5,15 @@ use wasm_bindgen::prelude::*;
 #[wasm_bindgen]
 pub fn today() -> String {
     let now: DateTime<Local> = Local::now();
-    now.format("%Y/%m/%d").to_string()
+    let day_of_week = match now.weekday() {
+        Weekday::Mon => "月",
+        Weekday::Tue => "火",
+        Weekday::Wed => "水",
+        Weekday::Thu => "木",
+        Weekday::Fri => "金",
+        Weekday::Sat => "土",
+        Weekday::Sun => "日",
+    };
+
+    format!("{}（{}）", now.format("%Y/%m/%d").to_string(), day_of_week)
 }
